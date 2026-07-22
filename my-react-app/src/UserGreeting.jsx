69bfac63bfb134b24c0d42b2f182549f1ee78b12
Shubhankar-Mohakud <shubhankar.mohakud@outlook.com>
2026-07-22 21:08:00 +0530

    conditional rendering

---
+++ my-react-app/src/UserGreeting.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+function UserGreeting({
+    isLoggedIn=false,
+    username="Guest",
+}) {
+
+    const welcomeMessage = <h2 className="welcome-message">Welcome {username}</h2>;
+    const loginPrompt = <h2 className="login-prompt">Please Login to continue</h2>
+
+    return (isLoggedIn ?  welcomeMessage : loginPrompt);
+}
+
+UserGreeting.PropTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+}
+
+export default UserGreeting;
